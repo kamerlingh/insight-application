6ac7b109d5c26f8b47b3ef315a019c6e0055e7cc
Kristen Sunter <kamerlingh@users.noreply.github.com>
2015-04-13 18:34:31 -0400

    Add PNG of linear regression

--- linear_regression.R
+++ linear_regression.R
@@ -1,4 +1,4 @@
-##plots with linear regression exported to png files, writes table of values to console
+##plots of sapphire data with linear regressions exported to png file
 
 #create linear regression models for deposition time and thickness
 dep_Al2O3.fit <- glm(Rs_inv ~ tdep, data=sample_data[which(sample_data$substrate=="Al2O3"),])
@@ -12,8 +12,17 @@ abline(dep_Al2O3.fit, col="blue")
 with(sample_data[which(sample_data$substrate=="Al2O3"),], plot(thickness, Rs_inv, col="blue", xlab="thickness (nm)", ylab=expression(paste("inverse sheet resistance ", (Omega/sq)^-1))))
 abline(thickness_Al2O3.fit, col="blue")
 
-#export plots with overlaid linear regression lines to png
+#export plots with overlaid linear regression lines to png with t values
+png("sapphire_analysis.png", width=500, height=1000, units = "px")
+par(mfrow=c(2,1))
+with(sample_data[which(sample_data$substrate=="Al2O3"),], plot(tdep, Rs_inv, col="blue", xlab="deposition time (s)", ylab=expression(paste("inverse sheet resistance ", (Omega/sq)^-1))))
+abline(dep_Al2O3.fit, col="blue")
+title(main="Inverse Sheet Resistance Vs. Deposition Time")
 
+with(sample_data[which(sample_data$substrate=="Al2O3"),], plot(thickness, Rs_inv, col="blue", xlab="thickness (nm)", ylab=expression(paste("inverse sheet resistance ", (Omega/sq)^-1))))
+abline(thickness_Al2O3.fit, col="blue")
+title(main="Inverse Sheet Resistance Vs. Measured Thickness")
+dev.off()
 
 
 
